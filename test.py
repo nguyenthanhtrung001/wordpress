4618ced99c72f48eb85bbe4dd94a5e37304d57b8
nguyenthanhtrung001 <thanhtrung8674@gmail.com>
2024-06-18 23:22:14 +0700

    test1

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ def hello_world():
     """
     Hàm để in ra chuỗi 'Hello, world!'
     """
-    print("Hello, world!")
+    print("Hello, world!1")
 
 
 # Gọi hàm hello_world để in ra chuỗi "Hello, world!"
